5201446a5f641ef08b1b758b6fe7bb4c2a9a7c20
jsshin <tb25271@gmail.com>
2023-08-25 21:47:22 +0900

    Logger 모듈 세팅
    기본 로깅에 필요한 Logger 모듈 (nest-pino,pino-http,pino-pretty) 설치후 app-module에 적용

--- apps/app-api/src/app-api.module.ts
+++ apps/app-api/src/app-api.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppApiController } from './app-api.controller';
 import { AppApiService } from './app-api.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 /**   Incoming request
  *    -> Middleware -> Guards -> Interceptors
@@ -11,12 +13,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
-      host: '',
-      username: '',
-      password: '',
-      database: '',
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
       entities: [],
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        customProps: (req, res) => ({
+          context: 'HTTP',
+        }),
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
     }),
   ],
   controllers: [AppApiController],
--- apps/app-api/src/app-api.service.ts
+++ apps/app-api/src/app-api.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 
 @Injectable()
 export class AppApiService {
+  constructor(
+    @InjectPinoLogger(AppApiService.name)
+    private readonly logger: PinoLogger,
+  ) {}
   getHello(): string {
+    this.logger.trace({ foo: 'bar' }, 'baz %s', 'qux');
+    this.logger.debug('foo %s %o', 'bar', { baz: 'qux' });
+    this.logger.info('foo');
     return 'Hello World!';
   }
 }
