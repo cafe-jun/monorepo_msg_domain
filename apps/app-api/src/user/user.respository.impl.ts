6ebc6bffe676d1acc32463ebd7a42f6a9653cbb5
jsshin <tb25271@gmail.com>
2023-08-27 22:50:16 +0900

    userRepository 인터페이스 정의후 Repository 정의
    정의된 UserRepository 에 따라 UserRepositoryImpl 후 Repository Layer 구현

---
+++ apps/app-api/src/user/user.respository.impl.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@app/entity/domain/user/user.entity';
+import { UserRepository } from './user.respository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { In, Repository } from 'typeorm';
+
+@Injectable()
+export class UserRepositoryImpl implements UserRepository {
+  constructor(
+    @InjectRepository(User)
+    private dataSource: Repository<User>,
+  ) {}
+  findOneByEmail(email: string): Promise<User> {
+    return this.dataSource.findOneBy({ email });
+  }
+  findOneById(id: number): Promise<User> {
+    return this.dataSource.findOneBy({ id });
+  }
+  findByIds(ids: number[]): Promise<User[]> {
+    return this.dataSource.findBy({
+      id: In(ids),
+    });
+  }
+  save(entity: User): Promise<User> {
+    return this.dataSource.save(entity);
+  }
+  async update(id: number, entity: Partial<User>): Promise<void> {
+    await this.dataSource.update(id, entity);
+    return;
+  }
+}
