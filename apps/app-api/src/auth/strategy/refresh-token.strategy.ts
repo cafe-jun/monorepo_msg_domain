5de9f39f1a86b5b07b0f508532ea93d7bd87da36
jsshin <tb25271@gmail.com>
2023-09-04 20:34:48 +0900

    Passport Module 라이브러리 설치 및 Jwt Token 정의 및 작성
    Passport Module 라이브러리 설치 및 jwt Token  Strategy 정의

---
+++ apps/app-api/src/auth/strategy/refresh-token.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from '../jwt/jwt-payload';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+      ignoreExpiration: false,
+    });
+  }
+
+  async validate(req: Request, payload: JwtPayload) {
+    const refreshToken = req.get('Authorization').replace('Bearer', '').trim();
+    return { ...payload, refreshToken };
+  }
+}
