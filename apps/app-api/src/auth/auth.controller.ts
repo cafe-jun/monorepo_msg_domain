5bd9956779b86bb3a3c4c9a4b9f77a60fa7cb4a2
jsshin <tb25271@gmail.com>
2023-09-13 20:22:31 +0900

    auth module controller 테스트 코드 작성
    auth module controller 테스트 코드 작성후 테스트 통과까지 확인

--- apps/app-api/src/auth/auth.controller.ts
+++ apps/app-api/src/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+import { JwtGuard } from './guard/jwt.guard';
 import { MsgToken } from './jwt/msg-token';
 import {
   Body,
@@ -6,12 +7,15 @@ import {
   HttpStatus,
   Inject,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserSignUpDto } from '../user/dto/user-signup.dto';
 import { User } from '@app/entity/domain/user/user.entity';
 import { AuthServiceImpl } from './auth.service.impl';
 import { UserSignInDto } from '../user/dto/user-signin.dto';
+import { CurrentUser } from './decorator/current-user.decorater';
+import { JwtRefreshGuard } from './guard/jwt-refresh.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -29,5 +33,19 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   async signin(@Body() signinDto: UserSignInDto): Promise<MsgToken> {
     return await this.authService.signin(signinDto);
+  }
+
+  @Post('signout')
+  @UseGuards(JwtGuard)
+  @HttpCode(HttpStatus.OK)
+  async signout(@CurrentUser('sub') sub: number): Promise<void> {
+    return await this.authService.signout(sub);
+  }
+
+  @Post('refresh-token')
+  @UseGuards(JwtRefreshGuard)
+  async refreshToken(@CurrentUser() user): Promise<MsgToken> {
+    const { sub: id, email, refreshToken } = user;
+    return await this.authService.refreshToken(id, email, refreshToken);
   }
 }
